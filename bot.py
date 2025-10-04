b8d1fb274d835c34ab7fb5cbb24b7604c1555c6a
boomchikchik <bulkdeals99@gmail.com>
2025-10-04 13:15:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
+from pyrogram import Client, idle
+from pyromod import Client as modClient
 
+from vars import *
+
+class Zoro(Client):
+    def __init__(self):
+        self.bot: modClient = modClient(
+            "Zoro",
+            API_ID,
+            API_HASH,
+            plugins=dict(root="plugins"),
+            bot_token=BOT_TOKEN
+        )
+        
+
+    async def start_bot(self):
+        await self.bot.start()
+        print(f"@{self.bot.me.username} is alive now!")
+        
+    async def stop(self):
+        await self.bot.stop()
+        print("Bot is dead now")
+
+    async def start_up(self):
+        await self.start_bot()
+        await idle()
+
+
+marimo = Zoro()
