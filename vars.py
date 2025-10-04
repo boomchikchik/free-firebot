27ccce3b3592201a106e7ac3dd533cbc60ab98b2
boomchikchik <bulkdeals99@gmail.com>
2025-10-04 13:46:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 
 
-API_ID = ""
-API_HASH = "" 
+API_ID = 21217717
+API_HASH = "c16e802d00b22ecd90681e0eb44abd11" 
 BOT_TOKEN = ""
 MONGO_URI = "mongodb+srv://new-user31:Qwerty_1234@referbot.up0ih5x.mongodb.net/?retryWrites=true&w=majority&appName=referbot"   #DATABASE URL 
